cba5bbe4fbdad9f7129fa7f8d73a1009d9a47b9f
quinnj <quinn.jacobd@gmail.com>
2017-05-12 08:33:08 -0600

    Initial commit

--- src/Null.jl
+++ src/Null.jl
@@ -1,5 +1,90 @@
 module Null
 
-# package code goes here
+importall Base.Operators
+
+export null, ?
+
+struct NullType end
+
+const null = NullType()
+
+Base.show(io::IO, x::NullType) = print(io, "null")
+
+?{T}(::Type{T}) = Union{T, NullType}
+*(::typeof(?), x) = ?(x)
+Base.isnull(v::NullType) = true
+
+Base.length(x::NullType) = 1
+Base.size(x::NullType) = ()
+Base.size(x::NullType, i::Integer) = i < 1 ? throw(BoundsError()) : 1
+Base.ndims(x::NullType) = 0
+Base.getindex(x::NullType, i) = i == 1 ? null : throw(BoundsError())
+
+Base.promote_rule{T}(::Type{T}, ::Type{NullType} ) = Union{T, NullType}
+
+# Comparison operators
+==(::NullType, ::NullType) = true
+==(::NullType, b) = false
+==(a, ::NullType) = false
+<(::NullType, ::NullType) = false
+<(::NullType, b) = false
+<(a, ::NullType) = true
+
+# Unary operators/functions
+for f in (:(Base.:+), :(Base.:-), :(Base.:*), :(Base.:/),
+          :(Base.abs), :(Base.abs2), :(Base.sign),
+          :(Base.acos), :(Base.acosh), :(Base.asin), :(Base.asinh), :(Base.atan), :(Base.atanh),
+          :(Base.sin), :(Base.sinh), :(Base.cos), :(Base.cosh), :(Base.tan), :(Base.tanh),
+          :(Base.exp), :(Base.exp2), :(Base.expm1), :(Base.log), :(Base.log10), :(Base.log1p),
+          :(Base.log2), :(Base.exponent), :(Base.sqrt), :(Base.gamma), :(Base.lgamma),
+          :(Base.ceil), :(Base.floor), :(Base.round), :(Base.trunc)
+         )
+    @eval $(f)(d::NullType) = null
+end
+
+for f in (:(Base.iseven), :(Base.ispow2), :(Base.isfinite), :(Base.isinf), :(Base.isodd),
+          :(Base.isinteger), :(Base.isreal), :(Base.isempty))
+    @eval $(f)(d::NullType) = false
+end
+
+# Binary operators/functions
+for f in (:(Base.:+), :(Base.:-), :(Base.:*), :(Base.:/), :(Base.:^),
+          :(Base.div), :(Base.mod), :(Base.fld), :(Base.rem), :(Base.min), :(Base.max))
+    @eval begin
+        # Scalar with null
+        ($f)(::NullType, ::NullType) = null
+        ($f)(d::NullType, x::Number) = null
+        ($f)(d::Number, x::NullType) = null
+    end
+end
+
+# to avoid ambiguity warnings
+(^)(::NullType, ::Integer) = null
+
+# Bit operators
+(&)(::NullType, ::NullType) = null
+(&)(a::NullType, b::Bool) = b ? null : false
+(&)(b::Bool, a::NullType) = b ? null : false
+(|)(::NullType, ::NullType) = null
+(|)(a::NullType, b::Bool) = b ? true : null
+(|)(b::Bool, a::NullType) = b ? true : null
+Base.xor(::NullType, ::NullType) = null
+Base.xor(a::NullType, b::Bool) = null
+Base.xor(b::Bool, a::NullType) = null
+
+# for f in (:(&), :(|), :(Base.xor))
+#     @eval begin
+#         # Scalar with null
+#         ($f)(::NullType, ::NullType) = null
+#         ($f)(::NullType, b::Integer) = null
+#     end
+# end
+
+replace(itr, a, b) = (ifelse(v == a, b, v) for v in itr)
+skip(itr, a=null) = (v for v in itr if v != a)
+
+# TODO
+ # create a series of benchmarks comparing DataArrays, NullableArrays, Vector{Union{NullType, T}}, and Vector{T}
+ # create use-case list for DataFrames: sql-like operations, pivot operations, by, dplyr
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,73 @@
-using Null
-using Base.Test
+using Base.Test, Null
 
-# write your own tests here
-@test 1 == 2
+@testset "Null" begin
+
+    const bit_operators = [&, |, ⊻]
+
+    const arithmetic_operators = [+, -, *, /, ^, Base.div, Base.mod, Base.fld, Base.rem]
+
+    const elementary_functions = [abs, abs2, sign,
+                                  acos, acosh, asin, asinh, atan, atanh, sin, sinh,
+                                  conj, cos, cosh, tan, tanh,
+                                  ceil, floor, round, trunc,
+                                  exp, exp2, expm1, log, log10, log1p, log2,
+                                  exponent, sqrt, gamma, lgamma]
+
+    # All unary operators return null when evaluating null
+    for f in [+, -]
+        @test isnull(f(null))
+    end
+
+    # All elementary functions return null when evaluating null
+    for f in elementary_functions
+        @test isnull(f(null))
+    end
+
+    # Comparison operators
+    @test null == null
+    @test !(1 == null)
+    @test !(null == 1)
+    @test !(null != null)
+    @test 1 != null
+    @test null != 1
+    @test !(null < null)
+    @test !(null < 1)
+    @test 1 < null
+
+    # All arithmetic operators return null when operating on two null's
+    # All arithmetic operators return null when operating on a scalar and an null
+    # All arithmetic operators return null when operating on an null and a scalar
+    for f in arithmetic_operators
+        @test isnull(f(null, null))
+        @test isnull(f(1, null))
+        @test isnull(f(null, 1))
+    end
+
+    # All bit operators return null when operating on two null's
+    for f in bit_operators
+        @test isnull(f(null, null))
+    end
+
+    @test isnull(null & true)
+    @test isnull(true & null)
+    @test !(null & false)
+    @test !(false & null)
+    @test isnull(null | false)
+    @test isnull(false | null)
+    @test null | true
+    @test true | null
+    @test isnull(xor(null, true))
+    @test isnull(xor(true, null))
+    @test isnull(xor(null, false))
+    @test isnull(xor(false, null))
+
+    @test length(null) == 1
+    @test size(null) == ()
+    @test size(null, 1) == 1
+    @test_throws BoundsError size(null, 0)
+    @test ndims(null) == 0
+    @test null[1] == null
+    @test_throws BoundsError null[2]
+
+    @test eltype([1, 2, null]) == ?Int
+end
