fca78d752d5ab634ee51325e03d11532397b3105
Bogumił Kamiński <bkamins@sgh.waw.pl>
2022-12-19 10:48:36 +0100

    Update Missings.jl

--- src/Missings.jl
+++ src/Missings.jl
@@ -497,5 +497,21 @@ and otherwise returns `f(x, args...; kwargs...)`, where `args` are following
 positional arguments passed to `f` and `kwargs` are its keyword arguments.
 
 # Examples
+```
+julia> emptymissing(last)([])
+missing
+
+julia> emptymissing(last)([1, 2, 3])
+3
+
+julia> emptymissing(first)([], 2)
+missing
+
+julia> emptymissing(first)([1], 2)
+1-element Vector{Int64}:
+1
+```
+"""
+emptymissing(f) = (x, args...; kwargs...) -> isempty(x) ? missing : f(x, args...; kwargs...)
 
 end # module
